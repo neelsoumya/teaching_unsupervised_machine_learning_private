a6b6e94568c058cad76736aee3e29e5edbb60ff4
soumyabanerjee <soumyabanerjee@soumyabanerjees-MacBook-Pro-1357.local>
2024-12-17 16:12:19 +0000

    edits automated git

--- code/US Arrests.R
+++ code/US Arrests.R
@@ -1,3 +1,12 @@
+heatmap assignment
+bulk data use clustering Sharday data
+day 2 cover math aspects of loadings
+picture assignment do PCA of picture
+email matt pay
+dimensionakity nonlinear
+detect outiers and bias using PCA
+
+
 library(factoextra)
 data("USArrests")
 USArrests.scaled <-scale(USArrests)
