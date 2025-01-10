a584f379123fa3626250761974baaa8171d073c7
soumyabanerjee <soumyabanerjee@soumyabanerjees-MacBook-Pro-1357.local>
2025-01-10 11:40:38 +0000

    edits automated git

--- practical/Chapter+10+Labs.R
+++ practical/Chapter+10+Labs.R
@@ -54,18 +54,28 @@ set.seed(2)
 x=matrix(rnorm(50*2), ncol=2)
 x[1:25,1]=x[1:25,1]+3
 x[1:25,2]=x[1:25,2]-4
-km.out=kmeans(x,2,nstart=20)
+km.out=kmeans(x,centers=2,nstart=20)
 km.out$cluster
-plot(x, col=(km.out$cluster+1), main="K-Means Clustering Results with K=2", xlab="", ylab="", pch=20, cex=2)
+
+plot(x)
+
+plot(x, col=(km.out$cluster), main="K-Means Clustering Results with K=2", xlab="", ylab="", pch=20, cex=2)
+
 set.seed(4)
-km.out=kmeans(x,3,nstart=20)
+km.out=kmeans(x,centers=3,nstart=20)
 km.out
-plot(x, col=(km.out$cluster+1), main="K-Means Clustering Results with K=3", xlab="", ylab="", pch=20, cex=2)
+
+plot(x, col=(km.out$cluster), main="K-Means Clustering Results with K=3", xlab="", ylab="", pch=20, cex=2)
+
 set.seed(3)
-km.out=kmeans(x,3,nstart=1)
+km.out=kmeans(x,centers=3,nstart=1)
 km.out$tot.withinss
-km.out=kmeans(x,3,nstart=20)
+plot(x, col=(km.out$cluster), main = "K-Means clustering results with K=2 and nstart=1", xlab="", ylab="", pch=20, cex=2)
+
+km.out=kmeans(x,centers=3,nstart=20)
 km.out$tot.withinss
+plot(x, col=(km.out$cluster), main = "K-Means clustering results with K=2 and nstart=20", xlab="", ylab="", pch=20, cex=2)
+
 
 # Hierarchical Clustering
 
