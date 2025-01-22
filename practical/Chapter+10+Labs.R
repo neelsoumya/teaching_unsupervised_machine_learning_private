4e48f042f30abc5cee34be5f6ccd656b3fb2d52d
soumyabanerjee <soumyabanerjee@soumyabanerjees-MacBook-Pro-1357.local>
2025-01-22 10:55:12 +0000

    edits automated git

--- practical/Chapter+10+Labs.R
+++ practical/Chapter+10+Labs.R
@@ -86,20 +86,26 @@ km.out=kmeans(x,centers=3,nstart=20)
 km.out$tot.withinss
 plot(x, col=(km.out$cluster), main = "K-Means clustering results with K=2 and nstart=20", xlab="", ylab="", pch=20, cex=2)
 
-
+############################
 # Hierarchical Clustering
+############################
+
+dist(x)
 
 hc.complete=hclust(dist(x), method="complete")
 hc.average=hclust(dist(x), method="average")
 hc.single=hclust(dist(x), method="single")
+
 par(mfrow=c(1,3))
 plot(hc.complete,main="Complete Linkage", xlab="", sub="", cex=.9)
 plot(hc.average, main="Average Linkage", xlab="", sub="", cex=.9)
 plot(hc.single, main="Single Linkage", xlab="", sub="", cex=.9)
+
 cutree(hc.complete, 2)
 cutree(hc.average, 2)
 cutree(hc.single, 2)
 cutree(hc.single, 4)
+
 xsc=scale(x)
 plot(hclust(dist(xsc), method="complete"), main="Hierarchical Clustering with Scaled Features")
 x=matrix(rnorm(30*3), ncol=3)
